98d71d641b473655319190890657edbef77264b5
CarrascoAlexis <acarrasco@gaming.tech>
2024-01-24 11:26:05 +0100

    Added router dom

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
+
+import '../../index.css';
 import './Header.css';
 
 function Header()
 {
 
-    return(<header>
+    return(
+    <header>
         <nav>
             <ul>
                 <li>a</li>
+                <li>b</li>
+            </ul>
+            <h1>TEST</h1>
+            <ul>
+                <li>c</li>
+                <li>d</li>
             </ul>
         </nav>
     </header>)
